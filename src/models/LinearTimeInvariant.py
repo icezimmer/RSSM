5abc0303c95b0d803fb7902513a61033204f4115
Giuseppe <g.lombardi11@studenti.unipi.it>
2023-12-22 01:27:11 +0100

    construct A by its eigs

--- src/models/LinearTimeInvariant.py
+++ src/models/LinearTimeInvariant.py
@@ -4,17 +4,35 @@ import torch
 Define a model subclass of torch.nn.Module that implements a linear time-invariant system.
 """
 class LinearTimeInvariant(torch.nn.Module):
-    def __init__(self, A, B, C, D, eps):
+    def __init__(self, eigs_A, B, C, D, eps):
         """
         Initialize the state-space model with system matrices A, B, C, D.
         These matrices define the dynamics of the system.
         """
         super(LinearTimeInvariant, self).__init__()
-        self.A = torch.tensor(A, dtype=torch.float32)
+        self.A = self.__state_matrix(eigs_A)
         self.B = torch.tensor(B, dtype=torch.float32)
         self.C = torch.tensor(C, dtype=torch.float32)
         self.D = torch.tensor(D, dtype=torch.float32)
         self.eps = torch.tensor([eps], dtype=torch.float32)
+
+    def __state_matrix(self, eigs):
+        """
+        Compute the state matrix of the system.
+        """
+        A = torch.empty((0,0), dtype=torch.float32)
+        
+        for item in list(eigs.items()):
+            re = item[0][0]
+            im = item[0][1]
+            mul = item[1]
+
+            for i in range(mul):
+                block = torch.tensor([[re, im], [-im, re]])
+                A = torch.block_diag(A, block)
+                
+        return A
+        
 
     def __discretize(self):
         """
