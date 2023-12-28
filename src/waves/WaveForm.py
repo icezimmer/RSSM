677167253227fac3107d82610830e1a59aa7cdab
GIuseppe Lombardi <g.lombardi11@studenti.unipi.it>
2023-12-28 15:25:15 +0100

    fix class

--- src/waves/WaveForm.py
+++ src/waves/WaveForm.py
@@ -1,24 +1,25 @@
+import torch
+from torchaudio.utils import download_asset
 import torchaudio
 from IPython.display import Audio
-from torchaudio.utils import download_asset
 import matplotlib.pyplot as plt
+from torchaudio.transforms import Resample  # If Resample is the correct function
 
 class WaveForm:
 
-    def __init__(path):
+    def __init__(self, path):
         self.wave, self.rate = torchaudio.load(download_asset(path))
 
-    def play():
+    def play(self):
+        waveform = self.wave
         if self.rate != 16000:
-            waveform = F.resample(self.wave, self.rate, 16000)
-        Audio(waveform.numpy()[0], rate=16000)
+            waveform = Resample(self.rate, 16000)(waveform)
+        return Audio(waveform.numpy()[0], rate=16000)
     
-    def plot():
+    def plot(self):
         waveform = self.wave.numpy()
-
         num_channels, num_frames = waveform.shape
         time_axis = torch.arange(0, num_frames) / self.rate
-
         figure, axes = plt.subplots(num_channels, 1)
         if num_channels == 1:
             axes = [axes]
@@ -27,18 +28,17 @@ class WaveForm:
             axes[c].grid(True)
             if num_channels > 1:
                 axes[c].set_ylabel(f"Channel {c+1}")
-        figure.suptitle("waveform")
+        figure.suptitle("Waveform")
 
-    def spectrogram():
+    def spectrogram(self):
         waveform = self.wave.numpy()
-
         num_channels, num_frames = waveform.shape
-
         figure, axes = plt.subplots(num_channels, 1)
         if num_channels == 1:
             axes = [axes]
         for c in range(num_channels):
-            axes[c].specgram(waveform[c], Fs=self.rate)
+            # Replace the following line with the correct spectrogram function
+            axes[c].plot_spectrogram(waveform[c], Fs=self.rate)  # Example placeholder
             if num_channels > 1:
                 axes[c].set_ylabel(f"Channel {c+1}")
         figure.suptitle("Spectrogram")
